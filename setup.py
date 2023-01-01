c029c3b128bc3ff58b8c6ea5e6409e029079662a
Peijin Zhang <peijin@ustc.edu>
2023-01-02 00:20:58 +0200

    packaging

--- setup.py
+++ setup.py
@@ -6,7 +6,20 @@ install_requires = [
 
 setuptools.setup(
     name="pymsoverview",
-    version="0.0.1",
+    version="0.0.2",
+    author = 'Peijin',  
     packages=setuptools.find_packages(),
     install_requires=install_requires,
-    )
+    entry_points = {
+        'console_scripts': [
+            'pymsoverview = pymsoverview.__main__:main',
+        ],
+    },  
+    classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',     # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license,
+    'Programming Language :: Python :: 3.9',
+    ],
+)
